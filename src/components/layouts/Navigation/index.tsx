e6f06baa30f636e49c5beb8fe94605578542a55e
Ryunosuke Onda <60596905+ryu-liam@users.noreply.github.com>
2022-10-31 02:15:33 +0900

    navのスタイル調整

--- src/components/layouts/Navigation/index.tsx
+++ src/components/layouts/Navigation/index.tsx
@@ -44,8 +44,12 @@ export const Nav = (props: Props) => {
                       src="/bound-logo/logo.png"
                       alt="Bound Logo"
                     />
+                    <div className="ml-2 font-bold text-xl sm:block hidden">
+                      Bound.xyz
+                    </div>
                   </div>
-                  <div className="hidden sm:ml-6 sm:block">
+
+                  <div className="hidden sm:ml-6 sm:flex items-center">
                     <div className="flex space-x-4">
                       {navigationMenus.map((item) => (
                         <a
@@ -54,7 +58,7 @@ export const Nav = (props: Props) => {
                           className={concatClassNames(
                             item.name === props.activeMenu
                               ? "bg-gray-900 text-white"
-                              : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                              : "text-gray-600 hover:bg-gray-700 hover:text-white",
                             "px-3 py-2 rounded-md text-sm font-medium"
                           )}
                           aria-current={
@@ -68,14 +72,6 @@ export const Nav = (props: Props) => {
                   </div>
                 </div>
                 <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                  <button
-                    type="button"
-                    className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                  >
-                    <span className="sr-only">View notifications</span>
-                    <BellIcon className="h-6 w-6" aria-hidden="true" />
-                  </button>
-
                   {/* Profile dropdown */}
                   <Menu as="div" className="relative ml-3">
                     <div>
