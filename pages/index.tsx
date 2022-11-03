c342b7031d96ee74b2a88988b573ccd686e348e6
Ryunosuke Onda <60596905+ryu-liam@users.noreply.github.com>
2022-11-04 02:17:04 +0900

    ToppageにProject一覧を表示

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,55 @@
-import { useAddress, useDisconnect, useUser, useLogin, useLogout, useMetamask } from '@thirdweb-dev/react';
-import type { NextPage } from 'next';
+import { PostgrestResponse } from "@supabase/supabase-js";
+import { useAddress, useUser } from "@thirdweb-dev/react";
+import type { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
+import { ProjectCard } from "src/components/ProjectCard";
+import { supabase } from "src/libs/supabase/client";
+import { Database } from "src/types/supabase";
 
-const Home: NextPage = () => {
+const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
+  projects,
+}) => {
   const address = useAddress();
-  const connect = useMetamask();
-  const disconnect = useDisconnect();
-
-  const login = useLogin();
-  const logout = useLogout();
   const { user } = useUser();
 
   return (
-    <div>
+    <div className="">
       {address ? (
         <>
-          <button onClick={disconnect}>Disconnect Wallet</button>
-          <button onClick={() => login()}>Login with Wallet</button>
-          <button onClick={logout}>Logout</button>
           <p>Your address: {address}</p>
           <pre>User: {JSON.stringify(user || null, undefined, 2)}</pre>
         </>
-      ) : (
-        <button onClick={connect}>Connect Wallet</button>
-      )}
+      ) : null}
+
+      <div className="p-10">
+        <h2 className="font-bold text-3xl text-center">Projects</h2>
+        <p className="mt-4 text-center textlg">
+          これまでのスキルを活かせる新しいプロジェクトに参加してみよう！
+        </p>
+
+        <div className="mt-10 flex">
+          {projects.data?.length
+            ? projects.data.map((project) => <ProjectCard project={project} />)
+            : null}
+        </div>
+      </div>
     </div>
   );
+};
+
+export const getStaticProps: GetStaticProps<{
+  projects: PostgrestResponse<Database["public"]["Tables"]["projects"]["Row"]>;
+}> = async () => {
+  const projects = await supabase.from("projects").select("*");
+  if (projects.error) {
+    console.error(projects.error);
+  }
+
+  return {
+    props: {
+      projects: projects,
+    },
+    redirect: 60 * 60,
+  };
 };
 
 export default Home;
