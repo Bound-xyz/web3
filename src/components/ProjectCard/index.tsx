8def24831a03165c297c368c38f2e953b10e543f
Ryunosuke Onda <60596905+ryu-liam@users.noreply.github.com>
2022-11-04 02:09:57 +0900

    Projectのカードを作成

---
+++ src/components/ProjectCard/index.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Database } from "src/types/supabase";
+import { format } from "date-fns";
+
+type Props = {
+  project: Database["public"]["Tables"]["projects"]["Row"];
+};
+
+export const ProjectCard = ({ project }: Props) => (
+  <div className="flex flex-col p-4 shadow w-80 h-96 rounded-lg">
+    <div className="relative w-full h-24">
+      <Image
+        src={project.thumbnail_uri}
+        alt={`project image of ${project.name}`}
+        layout="fill"
+        objectFit="contain"
+      />
+    </div>
+
+    <div className="mt-6">
+      {format(new Date(project.start_date), "yyyy-MM-dd")}
+      <span className="m-2">-</span>
+      {format(new Date(project.end_date), "yyyy-MM-dd")}
+    </div>
+    <h3 className="mt-4 text-xl">{project.name}</h3>
+    {/* TODO hrefの修正 */}
+    <Link href={`${project.id}`} passHref>
+      <a className="mt-4">View More</a>
+    </Link>
+  </div>
+);
