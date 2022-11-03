0c092a2e7c7432f9e3b6657a25f4e5dd1ba265f9
Ryunosuke Onda <60596905+ryu-liam@users.noreply.github.com>
2022-11-04 01:20:10 +0900

    生成した型をclientに適用

--- src/libs/supabase/client.ts
+++ src/libs/supabase/client.ts
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
+import { Database } from "src/types/supabase";
 import { envs } from "../../configs/env/env";
 
-export const supabase = createClient(
+export const supabase = createClient<Database>(
   envs.SUPABASE_URL,
   envs.SUPABASE_SERVICE_ROLE
 );
