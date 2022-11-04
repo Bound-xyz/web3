28572ebf83fb5486989bd95d1ae12dccd7de37fb
YuriNakayama <yuri620620@gmail.com>
2022-11-05 00:52:35 +0900

    vcに変更

--- contract/bound_thirdweb/thirdweb-contracts/contracts/Contract.sol
+++ contract/bound_thirdweb/thirdweb-contracts/contracts/Contract.sol
@@ -1,128 +1,227 @@
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "@thirdweb-dev/contracts/base/ERC721Drop.sol";
+// import "hardhat/console.sol";
+import "erc721a/contracts/ERC721A.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
 
-contract Contract is ERC721Drop {
-    struct Project {
-        string userName;
+contract NFTCredential is ERC721A, Ownable {
+    // Max batch size for minting one time
+    uint256 private _maxBatchSize;
 
-        string rewards;
+    // Mapping from token Id to hashed credential ID
+    mapping(uint256 => string) private _ownedCredential;
 
-        string projectName;
-        string experience;
-        string projectPeriod;
-        string skill;
-    }
-
-    string public baseURI;
-    Project public project;
-
+    // Mapping from hashed credential Id to owner possession flag
+    mapping(string => mapping(address => bool)) private _credentialOwnerships;
 
     constructor(
-        Project memory _project, 
         string memory _name,
         string memory _symbol,
-        address _royaltyRecipient,
-        uint128 _royaltyBps,
-        address _primarySaleRecipient
-    )
-        ERC721Drop(
-            _name,
-            _symbol,
-            _royaltyRecipient,
-            _royaltyBps,
-            _primarySaleRecipient
-        )
-    {project = _project;}
-
-    function _baseURI() internal view override returns (string memory) {
-        return baseURI;
+        uint256 _newMaxBatchSize
+    ) ERC721A(_name, _symbol) {
+        _maxBatchSize = _newMaxBatchSize;
     }
 
-    function updateBaseURI(string memory baseURI_) public onlyOwner {
-        baseURI = baseURI_;
-    }
-
-    // Mapping from token ID to locked status
-    mapping(uint256 => bool) _locked;
-
-    /// @notice Emitted when the locking status is changed to locked.
-    /// @dev If a token is minted and the status is locked, this event should be emitted.
-    /// @param tokenId The identifier for a token.
-    event Locked(uint256 tokenId);
-
-    /// @notice Emitted when the locking status is changed to unlocked.
-    /// @notice currently SBT Contract does not emit Unlocked event
-    /// @dev If a token is minted and the status is unlocked, this event should be emitted.
-    /// @param tokenId The identifier for a token.
-    event Unlocked(uint256 tokenId);
-
-    /// @notice Returns the locking status of an Soulbound Token
-    /// @dev SBTs assigned to zero address are considered invalid, and queries
-    /// about them do throw.
-    /// @param tokenId The identifier for an SBT.
-    function locked(uint256 tokenId) external view returns (bool) {
-        require(ownerOf(tokenId) != address(0));
-        return _locked[tokenId];
-    }
-
-   
-   function safeMint(address to, uint256 tokenId) public onlyOwner {
-        require(balanceOf(to) == 0, "MNT01");
-        require(_locked[tokenId] != true, "MNT02");
-
-        _locked[tokenId] = true;
-        emit Locked(tokenId);
-
-        _safeMint(to, tokenId);
-    }
-
-    modifier IsTransferAllowed(uint256 tokenId) {
-        require(!_locked[tokenId]);
-        _;
-    }
-
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 tokenId
-    )
-        public virtual override IsTransferAllowed(tokenId) {
-        super.safeTransferFrom(
-            from,
-            to,
-            tokenId
+    function mintAndTransfer(
+        string memory _credentialId,
+        string memory _description,
+        address[] memory _toAddresses,
+        string[] memory _imageURIs,
+        string[] memory _externalURIs
+    ) public onlyOwner {
+        uint256 requestNum = _toAddresses.length;
+        require(requestNum > 0, "The _toAddresses is empty.");
+        require(
+            requestNum <= getMaxBatchSize(),
+            "The length of _toAddresses must be less than or equal to _maxBatchSize."
         );
-    }
-
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 tokenId,
-        bytes memory data
-    )
-        public virtual override IsTransferAllowed(tokenId) {
-        super.safeTransferFrom(
-            from,
-            to,
-            tokenId,
-            data
+        require(
+            requestNum == _imageURIs.length,
+            "The length of _toAddresses and _imageURIs are NOT same."
         );
+        require(
+            requestNum == _externalURIs.length,
+            "The length of _toAddresses and _externalURIs are NOT same."
+        );
+        for (uint i = 0; i < requestNum; i++) {
+            address _to = _toAddresses[i];
+            bool hasCredential = _credentialOwnerships[_credentialId][_to];
+            require(
+                !hasCredential,
+                "One or more recipient has had already the same credential."
+            );
+            require(_to != owner(), "_toAddresses must NOT be included OWNER.");
+        }
+
+        // put the next token ID down in the variable before the bulk mint
+        uint256 startTokenId = _nextTokenId();
+        _safeMint(owner(), requestNum);
+
+        // do bulk transfer to each specified address only for the minted tokens
+        uint256 tokenId = startTokenId;
+        for (uint i = 0; i < requestNum; i++) {
+            // update the credential ID mapping
+            _ownedCredential[tokenId] = _credentialId;
+            // transfer to the specified address
+            safeTransferFrom(owner(), _toAddresses[i], tokenId);
+            // update the token URI
+            _setTokenURI(
+                tokenId,
+                generateTokenURI(_description, _imageURIs[i], _externalURIs[i])
+            );
+            tokenId += 1;
+        }
     }
 
     function transferFrom(
         address from,
         address to,
         uint256 tokenId
-    )
-        public virtual override IsTransferAllowed(tokenId) {
-        super.safeTransferFrom(
-            from,
-            to,
-            tokenId
+    ) public override payable onlyOwner {
+        address _tokenOwner = ownerOf(tokenId);
+        require(
+            from == _tokenOwner,
+            "The from-address is NOT the token ID's owner."
         );
+
+        // Banned for transfering to OWNER,
+        // because to make sure that the status of credential ID mappings will not be complicated.
+        require(to != owner(), "The to-address must NOT be OWNER.");
+
+        string memory _credentialId = ownedCredential(tokenId);
+        bool _hasToAddr = _credentialOwnerships[_credentialId][to];
+        require(!_hasToAddr, "The to-address has the same credential already.");
+
+        super.transferFrom(from, to, tokenId);
+        // update the credential ID mappings
+        _credentialOwnerships[_credentialId][from] = false;
+        _credentialOwnerships[_credentialId][to] = true;
+    }
+
+    function updateNFT(
+        uint256 tokenId,
+        string memory _credentialId,
+        string memory _description,
+        string memory _imageURI,
+        string memory _externalURI
+    ) public onlyOwner {
+        // update the credential ID mappings
+        string memory _currentCredentialId = ownedCredential(tokenId);
+        bytes32 _currentHash = keccak256(bytes(_currentCredentialId));
+        bytes32 _newHash = keccak256(bytes(_credentialId));
+        if (_currentHash != _newHash) {
+            _ownedCredential[tokenId] = _credentialId;
+            address _tokenOwner = ownerOf(tokenId);
+            _credentialOwnerships[_currentCredentialId][_tokenOwner] = false;
+            _credentialOwnerships[_credentialId][_tokenOwner] = true;
+        }
+        // update the tokenURI
+        _setTokenURI(tokenId, generateTokenURI(_description, _imageURI, _externalURI));
+    }
+
+    function getMaxBatchSize() public view onlyOwner returns (uint256) {
+        return _maxBatchSize;
+    }
+
+    function setMaxBatchSize(uint256 _newMaxBatchSize) public onlyOwner {
+        _maxBatchSize = _newMaxBatchSize;
+    }
+
+    function ownedCredential(uint256 tokenId)
+        public
+        view
+        onlyOwner
+        returns (string memory)
+    {
+        require(_exists(tokenId), "The token ID does NOT exist.");
+        return _ownedCredential[tokenId];
+    }
+
+    function transferOwnership(address newOwner) public override onlyOwner {
+        // Banned for transfering ownership to a user who has this token already,
+        // because to make sure that the status of credential ID mappings will not be complicated.
+        require(balanceOf(newOwner) == 0, "newOwner's balance must be zero.");
+        super.transferOwnership(newOwner);
+    }
+
+    // To be soulbound NFT except owner operations.
+    function _beforeTokenTransfers(
+        address,
+        address,
+        uint256,
+        uint256
+    ) internal view override onlyOwner {}
+
+    function generateTokenURI(
+        string memory _description,
+        string memory _imageURI,
+        string memory _externalURI
+    ) internal view returns (string memory) {
+        bytes memory _attributes = abi.encodePacked('"attributes": []');
+        string memory json = Base64.encode(
+            abi.encodePacked(
+                '{"name": "',
+                name(),
+                '",'
+                '"description": "',
+                _description,
+                '",'
+                '"image": "',
+                _imageURI,
+                '",',
+                '"external_url": "',
+                _externalURI,
+                '",',
+                string(_attributes),
+                "}"
+            )
+        );
+        return string(abi.encodePacked("data:application/json;base64,", json));
+    }
+
+    // =============================================================
+    //   The followings are copied from ERC721URIStorage.sol
+    // =============================================================
+
+    // Optional mapping for token URIs
+    mapping(uint256 => string) private _tokenURIs;
+
+    /**
+     * @dev See {IERC721Metadata-tokenURI}.
+     */
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        require(_exists(tokenId));
+        string memory _tokenURI = _tokenURIs[tokenId];
+        string memory base = _baseURI();
+
+        // If there is no base URI, return the token URI.
+        if (bytes(base).length == 0) {
+            return _tokenURI;
+        }
+        // If both are set, concatenate the baseURI and tokenURI (via abi.encodePacked).
+        if (bytes(_tokenURI).length > 0) {
+            return string(abi.encodePacked(base, _tokenURI));
+        }
+
+        return super.tokenURI(tokenId);
+    }
+
+    /**
+     * @dev Sets `_tokenURI` as the tokenURI of `tokenId`.
+     *
+     * Requirements:
+     *
+     * - `tokenId` must exist.
+     */
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal {
+        require(_exists(tokenId), "URI set of nonexistent token");
+        _tokenURIs[tokenId] = _tokenURI;
     }
 }
-
-// {"userName": "testname","rewards": "10eth", "projectName": "testproject", "experience": "testexperience", "projectPeriod": "10", "skill": "スキル"}
