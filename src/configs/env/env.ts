f86015caef5a0cec81a76d0577d04b77dc78502e
Ryunosuke Onda <60596905+ryu-liam@users.noreply.github.com>
2022-11-06 23:52:52 +0900

    envのupdate

--- src/configs/env/env.ts
+++ src/configs/env/env.ts
@@ -1,5 +1,5 @@
 export const envs: { [key in string]: string } = {
-  SUPABASE_URL: process.env.SUPABASE_URL || "",
-  SUPABASE_SERVICE_ROLE: process.env.SUPABASE_SERVICE_ROLE || "",
+  SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+  SUPABASE_SERVICE_ROLE: process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE || "",
   BOUND_WALLET_ADMIN_PRIVATE_KEY: process.env.ADMIN_PRIVATE_KEY || "",
 };
